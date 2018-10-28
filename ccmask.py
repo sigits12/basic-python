f2cbe5e304a80678371e57422b82e5d5c17801be
sigits12 <supratmanto12@gmail.com>
2018-10-28 17:11:20 +0700

    revision ccmask easier to understand

--- ccmask.py
+++ ccmask.py
@@ -1,12 +1,8 @@
 def maskify(w):
-    a = []
-    for i in range(len(w)):
-        if i < len(w) - 4:
-            a.append("#")
-        else:
-            a.append(w[i])
-    a = ''.join(a)
-    return a
+    word = list(w)
+    for i in range(len(w) - 4):
+        word[i] = '#'
+    return ''.join(word)
 
 
 print(maskify("assdk4"))
